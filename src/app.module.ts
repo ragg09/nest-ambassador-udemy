96d647ce5aad1a76e4c1da0161d4d45f9b8038ce
ragg09 <ragunayon@gmail.com>
2024-06-18 11:02:57 +0800

    Initial to Database

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'ambassador-db',
+      port: 3306,
+      username: 'ambassador-db',
+      password: 'Password123',
+      database: 'ambassador-db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
