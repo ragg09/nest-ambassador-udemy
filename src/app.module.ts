8f97b75257fe9ed18b941ff0e9c51c532f78b1f0
ragg09 <ragunayon@gmail.com>
2024-06-19 17:39:39 +0800

    Setup Order

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { ProductModule } from './product/product.module';
     UsersModule,
     AuthModule,
     ProductModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/order/order.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order';
+import { OrderItem } from './entities/order-item';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  controllers: [OrderController],
+  providers: [OrderService],
+})
+export class OrderModule {}
